d3e989c28c9d39fade92ffdc358dc58405cb7c2f
urbeck <74239018+urbeck@users.noreply.github.com>
2024-04-24 18:14:04 +0500

    Update name1.py

--- name1.py
+++ name1.py
@@ -4,13 +4,14 @@ window = display.set_mode((960, 520))
 clock = time.Clock()
 game = True
 
-lost_label = pygame.font.SysFont('arial', 25).render('TEST', True, (255,255,255))
+font.init()
+lost_label = font.Font('arial', 25).render('TEST', True, (255,255,255))
 
 while game:
   for i in event.get():
     if i.type == QUIT:
       game = False
     window.fill((125, 125, 0))
-    lost_label.blit()
+    window.blit(lost_label, (?,?))
     display.update()
     clock.tick(60)
